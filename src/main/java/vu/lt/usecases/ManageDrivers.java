1201472f753966d9c38db38b555d81903c54c6b9
Andrius Urbanavičius <urban.andrius@gmail.com>
2020-04-18 19:03:09 +0300

    Separated pages for managing cars and drivers

--- src/main/java/vu/lt/usecases/ManageDrivers.java
+++ src/main/java/vu/lt/usecases/ManageDrivers.java
@@ -2,11 +2,9 @@ package vu.lt.usecases;
 
 import lombok.Getter;
 import lombok.Setter;
-import vu.lt.entities.Car;
 import vu.lt.entities.Company;
 import vu.lt.entities.Driver;
 import vu.lt.interceptors.LoggedInvocation;
-import vu.lt.persistence.CarDAO;
 import vu.lt.persistence.CompanyDAO;
 import vu.lt.persistence.DriverDAO;
 
@@ -15,22 +13,23 @@ import javax.enterprise.inject.Model;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Map;
 
 @Model
-public class CompanyDetails {
+public class ManageDrivers {
 
     @Inject
     private CompanyDAO companyDAO;
-    @Inject
-    private CarDAO carDAO;
     @Inject
     private DriverDAO driverDAO;
 
     @Getter
     @Setter
     private Company company;
+
+    @Getter
+    @Setter
+    private Driver newDriver = new Driver();
 
     @PostConstruct
     private void init() {
@@ -41,39 +40,20 @@ public class CompanyDetails {
 
     @Transactional
     @LoggedInvocation
-    public String assignCarToDriver(Integer carId, Integer driverId) {
-        Car car = carDAO.findById(carId);
-        Driver driver = driverDAO.findById(driverId);
-        List<Car> carList = driver.getCarList();
-        boolean exists = false;
-        for (Car car1 : carList) {
-            if (car1.getId().equals(car.getId())) {
-                exists = true;
-                break;
-            }
-        }
-        if (!exists) {
-            driver.getCarList().add(car);
-        }
+    public String createDriver() {
+        newDriver.setCompany(company);
+        driverDAO.add(newDriver);
         return getReturnUrl();
     }
 
     @Transactional
     @LoggedInvocation
-    public String removeCarFromDriver(Integer carId, Integer driverId) {
-        Driver driver = driverDAO.findById(driverId);
-        List<Car> carList = driver.getCarList();
-
-        for (Car car1 : carList) {
-            if (car1.getId().equals(carId)) {
-                carList.remove(car1);
-                break;
-            }
-        }
+    public String removeDriver(Integer id) {
+        driverDAO.remove(id);
         return getReturnUrl();
     }
 
     private String getReturnUrl() {
-        return "companyDetails?faces-redirect=true&companyId=" + this.company.getId();
+        return "manageDrivers?faces-redirect=true&companyId=" + this.company.getId();
     }
 }
